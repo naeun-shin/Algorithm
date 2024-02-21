939aa7c74ad6b42228db04359463ece34af50135
naeun-shin <3020naaeun@gmail.com>
2024-02-21 10:43:48 +0900

    전체 삭제

--- 2_average.js
+++
@@ -1,6 +0,0 @@
-function solution(arr) {
-  var answer = arr.reduce((a, c) => a + c, 0) / arr.length;
-  //   answer = answer / arr.length;
-  return answer;
-}
-console.log(solution([1, 2, 3, 4]));
